71042df3b19c2e2c71b32778cf1438062aeb3073
jangflier <dbstjr3805@gmail.com>
2024-05-28 17:31:08 +0900

    Enable HTTP Support for Server Setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({
 	path: process.env.NODE_ENV === "development" ? ".env.development" : ".env.production",
 });
+import http from "http";
 import https, { ServerOptions } from "https";
 import Express from "express";
 import Path from "path";
@@ -35,22 +36,35 @@ export default class Server {
 
 	public start() {
 		const httpsServerOptions = this.httpsServerOptions;
-		if (!httpsServerOptions) {
-			throw new Error(
-				"HTTPS server options are not properly initialized. Please check the TLS_KEY_PATH and TLS_CERT_PATH in your .env file!",
-			);
-		}
+		const portNumber = process.env.SERVER_PORT || 5443;
 
-		sequelize
-			.sync()
-			.then(() => {
-				const portNumber = process.env.SERVER_PORT || 5443;
-				https.createServer(httpsServerOptions, this.app).listen(portNumber, () => {
-					console.log(textColor("ligthgreen", `\nDatabase connection successful`));
-					console.log(textColor("ligthgreen", `Server listening on port ${portNumber}\n`));
-				});
-			})
-			.catch((err: string) => console.log(err));
+		if (httpsServerOptions) {
+			sequelize
+				.sync()
+				.then(() => {
+					https.createServer(httpsServerOptions, this.app).listen(portNumber, () => {
+						console.log(textColor("ligthgreen", "\nDatabase connection successful"));
+						console.log(textColor("ligthgreen", `HTTPS server listening on port ${portNumber}\n`));
+					});
+				})
+				.catch((err: Error) => console.error(err));
+		} else {
+			console.log(
+				textColor(
+					"lightyellow",
+					"HTTPS server options are not provided. Starting HTTP server instead.",
+				),
+			);
+			sequelize
+				.sync()
+				.then(() => {
+					http.createServer(this.app).listen(portNumber, () => {
+						console.log(textColor("ligthgreen", "\nDatabase connection successful"));
+						console.log(textColor("ligthgreen", `HTTP server listening on port ${portNumber}\n`));
+					});
+				})
+				.catch((err: Error) => console.error(err));
+		}
 	}
 
 	private setConfig() {
@@ -88,6 +102,7 @@ export default class Server {
 		const SESSION_COOKIE_SECRET = process.env.SESSION_COOKIE_SECRET;
 		if (!SESSION_COOKIE_SECRET)
 			throw new Error("SESSION_COOKIE_SECRET must be set in the .env file!");
+		const isHTTPS = this.httpsServerOptions !== undefined;
 		this.app.use(
 			session.default({
 				secret: SESSION_COOKIE_SECRET,
@@ -95,9 +110,9 @@ export default class Server {
 				saveUninitialized: false,
 				store: sessionStore,
 				cookie: {
-					sameSite: "strict",
+					sameSite: isHTTPS ? "strict" : "lax",
 					httpOnly: true,
-					secure: true,
+					secure: isHTTPS,
 					maxAge: 2 * 60 * 60 * 1000,
 				},
 			}),
@@ -108,18 +123,23 @@ export default class Server {
 		const TLS_KEY_PATH = process.env.TLS_KEY_PATH;
 		const TLS_CERT_PATH = process.env.TLS_CERT_PATH;
 
-		if (!TLS_KEY_PATH || !TLS_CERT_PATH) {
-			throw new Error("TLS_KEY_PATH and TLS_CERT_PATH must be set in the .env file!");
-		}
-
 		try {
-			const key = fs.readFileSync(TLS_KEY_PATH);
-			const cert = fs.readFileSync(TLS_CERT_PATH);
+			if (TLS_KEY_PATH && TLS_CERT_PATH) {
+				const key = fs.readFileSync(TLS_KEY_PATH);
+				const cert = fs.readFileSync(TLS_CERT_PATH);
 
-			this.httpsServerOptions = {
-				key,
-				cert,
-			};
+				this.httpsServerOptions = {
+					key,
+					cert,
+				};
+			} else {
+				console.log(
+					textColor(
+						"lightyellow",
+						"TLS_KEY_PATH and TLS_CERT_PATH must be set in the .env file. Starting HTTP server instead.",
+					),
+				);
+			}
 		} catch (error) {
 			console.error("Failed to load key or certificate! : ", error);
 		}
